a202fb3d79acc2b9298cb7c875a031fa48e07c91
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-07-16 20:42:41 +0200

    Added callback to setState to correct input works

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,40 +15,56 @@ export class App extends Component {
   async componentDidMount() {
     this.fetchImages();
   }
-
+// Poprawiony setState z callbackiem
+  
   fetchImages = async () => {
-    try {
-      const { inputSearch, page, per_page, key } = this.state;
-      console.log('Fetch - try', inputSearch);
-      const response = await fetch(
-        `https://pixabay.com/api/?q=${inputSearch}&${page}&key=${key}&image_type=photo&orientation=horizontal&${per_page}`
-      );
-      const data = await response.json();
-
-      this.setState(prevState => ({ ...prevState, images: data.hits }));
-    } catch (error) {
-      console.log('Error', error);
+  try {
+    const { inputSearch, page, per_page, key } = this.state;
+    console.log(inputSearch);
+    const response = await fetch(
+      `https://pixabay.com/api/?q=${inputSearch}&${page}&key=${key}&image_type=photo&orientation=horizontal&${per_page}`
+    );
+    const data = await response.json();
+    return data;
+    // this.setState(prevState => ({ ...prevState, images: data.hits }));
+  } catch (error) {
+    console.log('Error', error);
+  }
+};
+handleSubmit = e => {
+  e.preventDefault();
+  this.inputSearch = e.target.elements.inputSearch.value;
+  //console.log('fetch - nowe zdjęcia', this.inputSearch);
+  this.setState(
+    prevState => {
+      return {
+        ...prevState,
+        inputSearch: this.inputSearch,
+      };
+    },
+    async () => {
+      try {
+        const response = await this.fetchImages();
+        if (response.code !== 'ERR_NETWORK') {
+          console.log(response);
+          this.setState(prevState => ({
+            ...prevState,
+            images: response.hits,
+          }));
+        } else {
+          console.log(`${response.code}`);
+        }
+      } catch (error) {
+        console.log(`${error}`);
+      }
     }
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.inputSearch = e.target.elements.inputSearch.value;
-    console.log('fetch - nowe zdjęcia', this.inputSearch);
-    this.setState(prevState => ({
-      ...prevState,
-      inputSearch: this.inputSearch,
-    }));
-    console.log('inputsearch po handleSubmit', this.inputSearch);
-    this.fetchImages();
-    console.log('inputsearch po fetchImages', this.inputSearch);
-    // this.setState({ inputSearch: '' });
-  };
-  // handleChange(e) {
-  //   const { name, value } = e.target;
-  //   this.setState(prevState => ({ ...prevState, [name]: value }));
-  // }
-
+  );
+  //console.log('inputsearch po handleSubmit', this.inputSearch);
+  // this.fetchImages();
+  // this.setState({ inputSearch: '' });
+};
+  
+// koniec poprawionego setState z callbackiem
   render() {
     console.log(this.state.images, 'tutaj zaostała przekazana tablica obrazów');
     return (
@@ -62,3 +78,39 @@ export class App extends Component {
     );
   }
 }
+
+
+
+//linijki 19 do 50
+// fetchImages = async () => {
+//    try {
+//      const { inputSearch, page, per_page, key } = this.state;
+//      console.log('Fetch - try', inputSearch);
+//      const response = await fetch(
+//        `https://pixabay.com/api/?q=${inputSearch}&${page}&key=${key}&image_type=photo&orientation=horizontal&${per_page}`
+//      );
+//      const data = await response.json();
+//
+//      this.setState(prevState => ({ ...prevState, images: data.hits }));
+ //   } catch (error) {
+//      console.log('Error', error);
+//    }
+//  };
+//
+//  handleSubmit = e => {
+//    e.preventDefault();
+ //   this.inputSearch = e.target.elements.inputSearch.value;
+//    console.log('fetch - nowe zdjęcia', this.inputSearch);
+//    this.setState(prevState => ({
+ //     ...prevState,
+//      inputSearch: this.inputSearch,
+//    }));
+ //   console.log('inputsearch po handleSubmit', this.inputSearch);
+//    this.fetchImages();
+//    console.log('inputsearch po fetchImages', this.inputSearch);
+//    // this.setState({ inputSearch: '' });
+//  };
+//  // handleChange(e) {
+//  //   const { name, value } = e.target;
+//  //   this.setState(prevState => ({ ...prevState, [name]: value }));
+//  // }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -20,21 +20,4 @@ ImageGallery.propTypes = {
   name: PropTypes.string,
 };
 
-// import PropTypes from 'prop-types';
-// export const ImageGalleryItem = ({ id, webformatURL, largeImageURL, tags }) => {
-//   return (
-//     <li key={image.id} class="gallery-item">
-//       <img
-//         src={image.webformatURL}
-//         alt={image.tags}
-//         large={image.largeImageURL}
-//       />
-//     </li>
-//   );
-// };
-// ImageGalleryItem.propTypes = {
-//   id: PropTypes.number,
-//   webformatURL: PropTypes.string,
-//   alt: PropTypes.string,
-//   largeImageURL: PropTypes.string,
-// };
+
