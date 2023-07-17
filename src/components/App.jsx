170297ea03fd3d5dcaeafead47d75efcf6519959
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-07-17 12:54:09 +0200

    Edit App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,56 +15,56 @@ export class App extends Component {
   async componentDidMount() {
     this.fetchImages();
   }
-// Poprawiony setState z callbackiem
-  
+  // Poprawiony setState z callbackiem
+
   fetchImages = async () => {
-  try {
-    const { inputSearch, page, per_page, key } = this.state;
-    console.log(inputSearch);
-    const response = await fetch(
-      `https://pixabay.com/api/?q=${inputSearch}&${page}&key=${key}&image_type=photo&orientation=horizontal&${per_page}`
-    );
-    const data = await response.json();
-    return data;
-    // this.setState(prevState => ({ ...prevState, images: data.hits }));
-  } catch (error) {
-    console.log('Error', error);
-  }
-};
-handleSubmit = e => {
-  e.preventDefault();
-  this.inputSearch = e.target.elements.inputSearch.value;
-  //console.log('fetch - nowe zdjęcia', this.inputSearch);
-  this.setState(
-    prevState => {
-      return {
-        ...prevState,
-        inputSearch: this.inputSearch,
-      };
-    },
-    async () => {
-      try {
-        const response = await this.fetchImages();
-        if (response.code !== 'ERR_NETWORK') {
-          console.log(response);
-          this.setState(prevState => ({
-            ...prevState,
-            images: response.hits,
-          }));
-        } else {
-          console.log(`${response.code}`);
-        }
-      } catch (error) {
-        console.log(`${error}`);
-      }
+    try {
+      const { inputSearch, page, per_page, key } = this.state;
+      console.log(inputSearch);
+      const response = await fetch(
+        `https://pixabay.com/api/?q=${inputSearch}&${page}&key=${key}&image_type=photo&orientation=horizontal&${per_page}`
+      );
+      const data = await response.json();
+      return data;
+      // this.setState(prevState => ({ ...prevState, images: data.hits }));
+    } catch (error) {
+      console.log('Error', error);
     }
-  );
-  //console.log('inputsearch po handleSubmit', this.inputSearch);
-  // this.fetchImages();
-  // this.setState({ inputSearch: '' });
-};
-  
-// koniec poprawionego setState z callbackiem
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.inputSearch = e.target.elements.inputSearch.value;
+    //console.log('fetch - nowe zdjęcia', this.inputSearch);
+    this.setState(
+      prevState => {
+        return {
+          ...prevState,
+          inputSearch: this.inputSearch,
+        };
+      },
+      async () => {
+        try {
+          const response = await this.fetchImages();
+          if (response.code !== 'ERR_NETWORK') {
+            console.log(response);
+            this.setState(prevState => ({
+              ...prevState,
+              images: response.hits,
+            }));
+          } else {
+            console.log(`${response.code}`);
+          }
+        } catch (error) {
+          console.log(`${error}`);
+        }
+      }
+    );
+    //console.log('inputsearch po handleSubmit', this.inputSearch);
+    // this.fetchImages();
+    // this.setState({ inputSearch: '' });
+  };
+
+  // koniec poprawionego setState z callbackiem
   render() {
     console.log(this.state.images, 'tutaj zaostała przekazana tablica obrazów');
     return (
@@ -79,8 +79,6 @@ handleSubmit = e => {
   }
 }
 
-
-
 //linijki 19 do 50
 // fetchImages = async () => {
 //    try {
@@ -92,20 +90,20 @@ handleSubmit = e => {
 //      const data = await response.json();
 //
 //      this.setState(prevState => ({ ...prevState, images: data.hits }));
- //   } catch (error) {
+//   } catch (error) {
 //      console.log('Error', error);
 //    }
 //  };
 //
 //  handleSubmit = e => {
 //    e.preventDefault();
- //   this.inputSearch = e.target.elements.inputSearch.value;
+//   this.inputSearch = e.target.elements.inputSearch.value;
 //    console.log('fetch - nowe zdjęcia', this.inputSearch);
 //    this.setState(prevState => ({
- //     ...prevState,
+//     ...prevState,
 //      inputSearch: this.inputSearch,
 //    }));
- //   console.log('inputsearch po handleSubmit', this.inputSearch);
+//   console.log('inputsearch po handleSubmit', this.inputSearch);
 //    this.fetchImages();
 //    console.log('inputsearch po fetchImages', this.inputSearch);
 //    // this.setState({ inputSearch: '' });
